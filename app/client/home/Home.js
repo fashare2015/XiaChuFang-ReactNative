2193d02daea545b569da636b8945c5769b99e64f
金良善 <jinliangshan@jinliangshandeiMac.local>
2017-06-20 13:45:02 +0800

    update home

--- app/client/home/Home.js
+++ app/client/home/Home.js
@@ -75,6 +75,42 @@ export class HomeFragment extends Component {
           </View>
         </TouchableNativeFeedback>;
 
+      case 2:
+        return <TouchableNativeFeedback onPress={() => this._navWeb(href)}>
+          <View style={styles.itemType2}>
+            <Image style={{height: 180, justifyContent: 'center', alignItems: 'center'}} source={imgUrl? {uri: imgUrl}: require('../../res/drawable/banner1.jpg')}>
+              <Text style={styles.title}>{item.contents.title_1st}</Text>
+              <Text style={styles.desc}>{item.contents.title_2nd}</Text>
+            </Image>
+          </View>
+        </TouchableNativeFeedback>;
+
+      case 4:
+        return <TouchableNativeFeedback onPress={() => this._navWeb(href)}>
+          <View style={styles.itemType4}>
+            <Image style={{height: 330}}
+                   source={imgUrl? {uri: imgUrl}: require('../../res/drawable/banner1.jpg')}/>
+          </View>
+        </TouchableNativeFeedback>;
+
+      case 5:
+        return <TouchableNativeFeedback onPress={() => this._navWeb(href)}>
+          <View style={styles.itemType5}>
+            <Image style={{height: 180, marginBottom: 20}} source={imgUrl? {uri: imgUrl}: require('../../res/drawable/banner1.jpg')}/>
+            <View style={{flexDirection: 'row'}}>
+              <View style={{flex: 1}}>
+                <Text style={styles.title}>{item.contents.title}</Text>
+                <Text style={styles.desc}>{item.contents.desc}</Text>
+                <Text style={styles.desc}>{(item.contents.score !== ''? item.contents.score + '分' + ' · ': '') + item.contents.n_cooked + '人做过'}</Text>
+              </View>
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <Image style={{width: 50, height: 50, borderRadius: 30}} source={{uri: item.contents.author.photo}}/>
+                <Text style={styles.desc}>{item.contents.author.name}</Text>
+              </View>
+            </View>
+          </View>
+        </TouchableNativeFeedback>;
+
       case 9:
         return <TouchableNativeFeedback onPress={() => this._navWeb(href)}>
           <View style={styles.itemType9}>
@@ -83,7 +119,7 @@ export class HomeFragment extends Component {
             { item.contents.show_hottest_discussion?
               <View>
                 <Text style={styles.desc}>{item.contents.hottest_discussion.text}</Text>
-                <Text style={styles.desc}>{item.contents.hottest_discussion.author_name + ' . ' + item.contents.hottest_discussion.n_diggs + "赞"}</Text>
+                <Text style={styles.desc}>{item.contents.hottest_discussion.author_name + ' · ' + item.contents.hottest_discussion.n_diggs + "赞"}</Text>
               </View>
               :
               <Text style={styles.desc}>{item.contents.n_discussions + '个回答'}</Text>}
@@ -119,6 +155,24 @@ const styles = StyleSheet.create({
   },
 
   itemType1:{
+    padding: 20,
+    borderBottomColor:'#f2f2f2',//cell的分割线
+    borderBottomWidth:1
+  },
+
+  itemType2:{
+    padding: 20,
+    borderBottomColor:'#f2f2f2',//cell的分割线
+    borderBottomWidth:1
+  },
+
+  itemType5:{
+    padding: 20,
+    borderBottomColor:'#f2f2f2',//cell的分割线
+    borderBottomWidth:1
+  },
+
+  itemType4:{
     padding: 20,
     borderBottomColor:'#f2f2f2',//cell的分割线
     borderBottomWidth:1
